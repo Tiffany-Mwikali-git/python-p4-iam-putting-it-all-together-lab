fc8f25e889debc7df258539b59a751636f925b38
Tiffany <marcy.kilonzo@student.moringaschool.com>
2025-09-25 12:46:21 +0300

    lab

--- server/testing/conftest.py
+++ server/testing/conftest.py
@@ -1,9 +1,14 @@
-#!/usr/bin/env python3
+import pytest
+from app import app, db
 
-def pytest_itemcollected(item):
-    par = item.parent.obj
-    node = item.obj
-    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
-    suf = node.__doc__.strip() if node.__doc__ else node.__name__
-    if pref or suf:
-        item._nodeid = ' '.join((pref, suf))
+@pytest.fixture(scope="session")
+def test_app():
+    app.config.update(
+        SQLALCHEMY_DATABASE_URI="sqlite:///:memory:",
+        TESTING=True,
+        SECRET_KEY="test-secret",
+    )
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.drop_all()
